1a403ae5712ad208c9ee7730e35ef949ee2ebbd5
IluvRiver <leecm2468@gmail.com>
2025-08-18 10:38:48 +0900

    ssl true

--- backend/product-service/src/main/java/com/shop/product/config/RedisConfig.java
+++ backend/product-service/src/main/java/com/shop/product/config/RedisConfig.java
@@ -1,14 +1,53 @@
 package com.shop.product.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SslOptions;
 
 @Configuration
 public class RedisConfig {
+    
+    @Value("${spring.data.redis.host}")
+    private String host;
+    
+    @Value("${spring.data.redis.port}")
+    private int port;
+    
+    @Value("${spring.data.redis.password}")
+    private String password;
+    
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(host);
+        redisConfig.setPort(port);
+        redisConfig.setPassword(password);
+        
+        // SSL 설정
+        SslOptions sslOptions = SslOptions.builder()
+                .jdkSslProvider()
+                .build();
+        
+        ClientOptions clientOptions = ClientOptions.builder()
+                .sslOptions(sslOptions)
+                .build();
+        
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl()
+                .clientOptions(clientOptions)
+                .build();
+        
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
+    }
     
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
